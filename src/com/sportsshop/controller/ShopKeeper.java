729a206af99cae7e6535d87998e626df98ec1eed
sathishkumar187 <96241781+sathishkumar187@users.noreply.github.com>
2022-01-22 15:25:12 +0530

    Update ShopKeeper.java

--- src/com/sportsshop/controller/ShopKeeper.java
+++ src/com/sportsshop/controller/ShopKeeper.java
@@ -6,22 +6,21 @@ import com.sportsshop.service.ShopServices;
 import com.sportsshop.view.SportsShop;
 
 public class ShopKeeper  {
-	private static final ShopServices SHOP_SERVICE_IMPLEMENTATION = new ShopServiceImplementation();
+    private static final ShopServices SHOP_SERVICE_IMPLEMENTATION = new ShopServiceImplementation();
 	
-	public static void addProduct(String brand, Product product) {
-		SportsShop.showAllProducts(SHOP_SERVICE_IMPLEMENTATION.addProduct(brand, product));
-	}
+    public static void addProduct(String brand, Product product) {
+	SportsShop.showAllProducts(SHOP_SERVICE_IMPLEMENTATION.addProduct(brand, product));
+    }
 
-	public static void selectProduct(String brand, String name, char size) {
-		SportsShop.showSelectedProduct(SHOP_SERVICE_IMPLEMENTATION.selectProduct(brand, name, size));
-	}
+    public static void selectProduct(String brand, String name, char size) {
+	SportsShop.showSelectedProduct(SHOP_SERVICE_IMPLEMENTATION.selectProduct(brand, name, size));
+    }
 
-	public static void updateProductPrice(String brand, String name, char size, float price) {
-		SportsShop.showAllProducts(SHOP_SERVICE_IMPLEMENTATION.updateProductPrice(brand, name, size, price));
-	}
+    public static void updateProductPrice(String brand, String name, char size, float price) {
+	SportsShop.showAllProducts(SHOP_SERVICE_IMPLEMENTATION.updateProductPrice(brand, name, size, price));
+    }
 
-	public static void removeProduct(String brand, String name, char size) {
-		SportsShop.showAllProducts(SHOP_SERVICE_IMPLEMENTATION.removeProduct(brand, name, size));
-	}
-	
+    public static void removeProduct(String brand, String name, char size) {
+	SportsShop.showAllProducts(SHOP_SERVICE_IMPLEMENTATION.removeProduct(brand, name, size));
+    }	
 }
