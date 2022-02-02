106c98b86840bdc117924118a355ffdd2d091b6c
sathishkumar187 <96241781+sathishkumar187@users.noreply.github.com>
2022-02-02 19:03:32 +0530

    Update InvalidProductException.java

--- src/main/java/com/sportsshop/customexceptions/InvalidProductException.java
+++ src/main/java/com/sportsshop/customexceptions/InvalidProductException.java
@@ -2,4 +2,7 @@ package com.sportsshop.customexceptions;
 
 public class InvalidProductException extends RuntimeException {
 	
+    public InvalidProductException(String message) {
+        super(message);
+    }
 }
