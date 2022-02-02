b8edbe5c1f86d82937625968d1ace76395c9036c
sathishkumar187 <96241781+sathishkumar187@users.noreply.github.com>
2022-02-02 16:41:25 +0530

    Intentations

--- src/main/java/com/sportsshop/service/ShopServiceImplementation.java
+++ src/main/java/com/sportsshop/service/ShopServiceImplementation.java
@@ -14,82 +14,80 @@ import com.sportsshop.model.Product;
 public class ShopServiceImplementation implements ShopServices {
     private static final Map<String, List<Product>> SPORTS_KITS = new HashMap<String, List<Product>>();
 	
-	/**
-	 * It adds the product.
-	 */
-	public void addProduct(Product product) {
-		final List<Product> sportsProducts = new ArrayList<Product>();
+    /**
+     * It adds the product.
+     */
+    public void addProduct(Product product) {
+	final List<Product> sportsProducts = new ArrayList<Product>();
 		
-		if (!SPORTS_KITS.containsKey(product.getBrand())) {
-			sportsProducts.add(product);
-			SPORTS_KITS.put(product.getBrand(), sportsProducts);
-		} else {
-			SPORTS_KITS.get(product.getBrand()).add(product);
-		}
+	if (!SPORTS_KITS.containsKey(product.getBrand())) {
+       	    sportsProducts.add(product);
+  	    SPORTS_KITS.put(product.getBrand(), sportsProducts);
+	} else {
+	    SPORTS_KITS.get(product.getBrand()).add(product);
 	}
+    }    
 
-	/**
-	 * It selects the product.
-	 * @throws NullProductException 
-	 */
-	public Product selectProduct(Product product) throws InvalidProductException {
+    /**
+     * It selects the product.
+     * @throws NullProductException 
+     */
+    public Product selectProduct(Product product) throws InvalidProductException {
 		
-		if (SPORTS_KITS.containsKey(product.getBrand())) {
+	if (SPORTS_KITS.containsKey(product.getBrand())) {
 
-			for (Product productDetails : SPORTS_KITS.get(product.getBrand())) {
+            for (Product productDetails : SPORTS_KITS.get(product.getBrand())) {
 
-				if (productDetails.getBrand().equals(product.getBrand())
-						&& productDetails.getName().equals(product.getName())
-						&& productDetails.getSize() == product.getSize()) {
-					return productDetails;
-				}
-			}
-		} else {
-			throw new InvalidProductException();
-		}
-		return null;
+                if (productDetails.getBrand().equals(product.getBrand()) && productDetails.getName().equals(product.getName())
+			&& productDetails.getSize() == product.getSize()) {
+		    return productDetails;
+	        }
+            }
+	} else {
+	    throw new InvalidProductException();
 	}
+	return null;
+    }
 
-	/**
-	 * To update the product price.
-	 * @throws NullProductException 
-	 */
-	public Product updateProductPrice(Product product) throws InvalidProductException {
-		final Product productDetails = selectProduct(product);
+    /**
+     * To update the product price.
+     * @throws NullProductException 
+     */
+    public Product updateProductPrice(Product product) throws InvalidProductException {
+    final Product productDetails = selectProduct(product);
 		
-		if (productDetails != null) {
-			productDetails.setPrice(product.getPrice());
-			return productDetails;
-		} else {
-			throw new InvalidProductException();
-		}
-	}
+        if (productDetails != null) {
+	    productDetails.setPrice(product.getPrice());
+	    return productDetails;
+        } else {
+	    throw new InvalidProductException();
+        }
+    }    
 
-	/**
-	 * To remove the product.
-	 * @throws InvalidProductException 
-	 */
-	public Product removeProduct(Product product) throws InvalidProductException {
-		final Product productDetails = selectProduct(product);
+    /**
+     * To remove the product. 
+     * @throws InvalidProductException 
+     */
+    public Product removeProduct(Product product) throws InvalidProductException {
+	final Product productDetails = selectProduct(product);
 		
-		if (productDetails == null) {
-			SPORTS_KITS.get(product.getBrand()).remove(productDetails);
-			return productDetails;
-		} else {
-			throw new InvalidProductException();
-		}
+	if (productDetails == null) {
+	    SPORTS_KITS.get(product.getBrand()).remove(productDetails);
+	    return productDetails;
+	} else {
+	    throw new InvalidProductException();
 	}
+    }    
 	
-	/**
-	 * Select all the products.
-	 */
-	public List<Product> selectAllProducts() {
-		List<Product> allProducts = new ArrayList<>();
+    /**
+     * Select all the products.
+     */
+    public List<Product> selectAllProducts() {
+        List<Product> allProducts = new ArrayList<>();
 		
-		for (List<Product> products : SPORTS_KITS.values()) {
-			allProducts.addAll(products);
-		}
-		
-		return allProducts;
+        for (List<Product> products : SPORTS_KITS.values()) {
+	    allProducts.addAll(products);
 	}
+	return allProducts;
+    }
 }
