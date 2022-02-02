6c8db8f82e955172da68c11d4ed0e5b1d43fd12f
sathishkumar187 <96241781+sathishkumar187@users.noreply.github.com>
2022-02-02 17:14:52 +0530

    Intentations

--- src/main/java/com/sportsshop/dao/DBConnection.java
+++ src/main/java/com/sportsshop/dao/DBConnection.java
@@ -4,24 +4,24 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
-	private static final String JDBC_URL = "jdbc:postgresql://localhost:1818/SportsShop";
-	private static final String JDBC_USERNAME = "postgres";
-	private static final String JDBC_PASSWORD = "Ssk18187";
+    private static final String JDBC_URL = "jdbc:postgresql://localhost:1818/SportsShop";
+    private static final String JDBC_USERNAME = "postgres";
+    private static final String JDBC_PASSWORD = "Ssk18187";
     private static final String JDBC_DRIVER = "org.postgresql.Driver";
     
-	/**
-	 * It connects database.
-	 * @return established connection by connection.
-	 */
-	Connection getConnection() {
-		Connection connection = null;
+    /**
+     * It connects database.
+     * @return established connection by connection.
+     */
+    Connection getConnection() {
+	Connection connection = null;
 
-		try {
-			Class.forName(JDBC_DRIVER);
-			connection = DriverManager.getConnection(JDBC_URL, JDBC_USERNAME, JDBC_PASSWORD);
-		} catch(Exception e) {
-			System.out.println("Connection Failed \n    Please Check Connection");
-		}
-		return connection;
+	try {
+	    Class.forName(JDBC_DRIVER);
+	    connection = DriverManager.getConnection(JDBC_URL, JDBC_USERNAME, JDBC_PASSWORD);
+	} catch(Exception e) {
+	    System.out.println("Connection Failed \n    Please Check Connection");
 	}
+	return connection;
+    }
 }
